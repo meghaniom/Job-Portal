b44330cc3a83b4b84e5cf5bbb41c4a55e68d8d21
ommeghani <meghanio264@gmail.com>
2025-06-06 19:08:52 +0530

    xjhangte

--- src/pages/Joblist.jsx
+++ src/pages/Joblist.jsx
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const JobListings = () => {
+   const [data, setData] = useState();
+
+    useEffect(() => {
+      const fetchData = async() => {
+        const response = await fetch ('https://remotive.com/api/remote-jobs?category=software-dev');
+        const json = await response.json();
+        setData(json);
+      }
+      fetchData();
+
+
+      }
+    },[])
   return (
     <div className="min-h-screen bg-gray-100 p-8">
       <h2 className="text-2xl font-bold text-gray-800 mb-4">Job Listings</h2>
